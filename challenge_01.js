752f736c0b18182e6e5b7abc7d2410eb05232272
Peter <schmidt.multimedia@gmail.com>
2023-03-31 11:55:01 +0200

    Variante 2

--- challenge_01.js
+++ challenge_01.js
@@ -9,24 +9,40 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
 
-let ziffer1, ziffer2, zahl1, zahl2, summe;
+/*********** Variante 1  ****************/
+// Vorteil: gute Lesbarkeit | aber viele Variablen
 
-ziffer1 = prompt("Bitte Zahl 1 eingeben:");
-ziffer2 = prompt("Bitte Zahl 2 eingeben:");
+// let ziffer1, ziffer2, zahl1, zahl2, summe;
 
-console.log("Datentyp: " + typeof ziffer1);
-console.log("Datentyp: " + typeof ziffer2);
+// ziffer1 = prompt("Bitte Zahl 1 eingeben:");
+// ziffer2 = prompt("Bitte Zahl 2 eingeben:");
 
-// Typkonvertierung 1: ziffer --> zahl (mit Nachkomma)
-// zahl1 = parseFloat(ziffer1);
-// zahl2 = parseFloat(ziffer2);
+// console.log("Datentyp: " + typeof ziffer1);
+// console.log("Datentyp: " + typeof ziffer2);
 
-// Typkonvertierung 2: ziffer --> zahl (als Ganzzahl)
-zahl1 = parseInt(ziffer1);
-zahl2 = parseInt(ziffer2);
+// // Typkonvertierung 1: ziffer --> zahl (mit Nachkomma)
+// // zahl1 = parseFloat(ziffer1);
+// // zahl2 = parseFloat(ziffer2);
 
-console.log("Datentyp: " + typeof zahl1);
-console.log("Datentyp: " + typeof zahl2);
+// // Typkonvertierung 2: ziffer --> zahl (als Ganzzahl)
+// zahl1 = parseInt(ziffer1);
+// zahl2 = parseInt(ziffer2);
+
+// console.log("Datentyp: " + typeof zahl1);
+// console.log("Datentyp: " + typeof zahl2);
+
+// summe = zahl1 + zahl2;
+
+// console.log("Die Summe der Zahlen ist: " + summe);
+
+/*********** Variante 2  ****************/
+
+// Kompaktere Lösung
+
+let zahl1, zahl2, summe;
+
+zahl1 = parseInt(prompt("Zahl 1: "));
+zahl2 = parseInt(prompt("Zahl 2: "));
 
 summe = zahl1 + zahl2;
 
